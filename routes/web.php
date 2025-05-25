f6fe4af0a54c5a0224409cf2290ee6a2c22c59f7
Imam <imsdaiss@gmail.com>
2025-05-25 23:14:39 +0700

    Menambahkan file terbaru

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Homepage.home');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/regist', function () {
+    return view('regist');
+});
+
+Route::get('/browse', function () {
+    return view('browse');
+});
+
+Route::get('/create', function () {
+    return view('create');
+});
